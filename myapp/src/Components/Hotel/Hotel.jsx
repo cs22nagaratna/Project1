948261ceb8dcf27257de75c16490da7d304e2458
cs22nagaratna <cs22nagaratna@svpkumta.in>
2025-02-18 12:54:44 +0530

    first commit

--- myapp/src/Components/Hotel/Hotel.jsx
+++ myapp/src/Components/Hotel/Hotel.jsx
@@ -1,4 +1,10 @@
 import React from "react";
+import { GiMedicinePills } from "react-icons/gi";
+import { IoHomeOutline } from "react-icons/io5";
+import { FaGift } from "react-icons/fa";
+import { FaRegUser } from "react-icons/fa";
+import { FaSearch } from "react-icons/fa";
+import { FaArrowRightLong } from "react-icons/fa6";
 
 export const Hotel=()=>{
     return(
@@ -40,28 +46,36 @@ export const Hotel=()=>{
                             <div style={{flex:'7'}}>
                                 <p style={{fontFamily:'arial,sans serif',color:'yellow'}}>Faclities</p>
                             </div>
-                            <div style={{flex:'1'}}></div>
+                            <div style={{paddingLeft:'20px', flex:'2',paddingTop:'20px',backgroundColor:"darkgreen",borderRadius:'200px',margin:'5px',marginLeft:'180px',marginRight:'5px'}}>
+                                <FaArrowRightLong />
+                            </div>
                         </div>
                         <div style={{border:'1px solid black',flex:'1',display:'flex',borderRadius:'30px',margin:'5px'}}>
                             <div style={{flex:'1'}}></div>
                             <div style={{flex:'7'}}>
                                 <p style={{fontFamily:'arial,sans serif',color:'yellow'}}>Virtual Key</p>
                             </div>
-                            <div style={{flex:'1'}}></div>
+                            <div style={{paddingLeft:'20px', flex:'2',paddingTop:'20px',backgroundColor:"darkgreen",borderRadius:'200px',margin:'5px',marginLeft:'180px',marginRight:'5px'}}>
+                                <FaArrowRightLong />
+                            </div>
                         </div>
                         <div style={{border:'1px solid black',flex:'1',display:'flex',borderRadius:'30px',margin:'5px'}}>
                             <div style={{flex:'1'}}></div>
                             <div style={{flex:'7'}}>
                                 <p style={{fontFamily:'arial,sans serif',color:'yellow'}}>My Room</p>
                             </div>
-                            <div style={{flex:'1'}}></div>
+                            <div style={{paddingLeft:'20px', flex:'2',paddingTop:'20px',backgroundColor:"darkgreen",borderRadius:'200px',margin:'5px',marginLeft:'180px',marginRight:'5px'}}>
+                                <FaArrowRightLong />
+                            </div>
                         </div>
                         <div style={{border:'1px solid black',flex:'1',display:'flex',borderRadius:'30px',margin:'5px'}}>
                             <div style={{flex:'1'}}></div>
                             <div style={{flex:'7'}}>
                                 <p style={{fontFamily:'arial,sans serif',color:'yellow'}}>Order Room Service</p>
                             </div>
-                            <div style={{flex:'1'}}></div>
+                            <div style={{paddingLeft:'20px', flex:'2',paddingTop:'20px',backgroundColor:"darkgreen",borderRadius:'200px',margin:'5px',marginLeft:'180px',marginRight:'5px'}}>
+                                <FaArrowRightLong />
+                            </div>
                         </div>
                         <div style={{border:'1px solid black',borderBottom:'white', flex:'1',display:'flex',borderRadius:'30px 30px 0 0',marginRight:'5px',marginLeft:'5px'}}>
                             <div style={{flex:'0.5'}}></div>
@@ -86,19 +100,29 @@ export const Hotel=()=>{
                         </div>
                     <div style={{flex:'2',display:'flex'}}>
                         <div style={{flex:1,display:'flex',marginRight:'9px',marginLeft:'9px'}}>
-                            <div style={{border:'1px solid black',flex:'1',borderRadius:'80px',margin:'10px'}}></div>
+                            <div style={{border:'1px solid black',flex:'1',borderRadius:'80px',margin:'10px',textAlign:'center'}}>
+                                <FaRegUser />
+                            </div>
                         </div>
                         <div style={{flex:1,display:'flex',marginRight:'9px',marginLeft:'9px'}}>
-                            <div style={{border:'1px solid black',flex:'1',borderRadius:'80px',margin:'10px'}}></div>
+                            <div style={{border:'1px solid black',flex:'1',borderRadius:'80px',margin:'10px',textAlign:'center'}}>
+                                <GiMedicinePills />
+                            </div>
                         </div>
                         <div style={{flex:1,display:'flex',marginRight:'9px',marginLeft:'9px'}}>
-                            <div style={{border:'1px solid black',flex:'1',borderRadius:'80px',margin:'10px'}}></div>
+                            <div style={{border:'1px solid black',flex:'1',borderRadius:'80px',margin:'10px',textAlign:'center'}}>
+                            <IoHomeOutline />
+                            </div>
                         </div>
                         <div style={{flex:1,display:'flex',marginRight:'9px',marginLeft:'9px'}}>
-                            <div style={{border:'1px solid black',flex:'1',borderRadius:'80px',margin:'10px'}}></div>
+                            <div style={{border:'1px solid black',flex:'1',borderRadius:'80px',margin:'10px',textAlign:'center'}}>
+                            <FaGift />
+                            </div>
                         </div>
                         <div style={{flex:1,display:'flex',marginRight:'9px',marginLeft:'9px'}}>
-                            <div style={{border:'1px solid black',flex:'1',borderRadius:'80px',margin:'10px'}}></div>
+                            <div style={{border:'1px solid black',flex:'1',borderRadius:'80px',margin:'10px',textAlign:'center'}}>
+                            <FaSearch />
+                            </div>
                         </div>
                     </div>
                 </div>
