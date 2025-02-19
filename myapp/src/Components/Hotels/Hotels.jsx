f302367fa8fed1f6cdaf0e90b21663ad7ce9d4fd
cs22nagaratna <cs22nagaratna@svpkumta.in>
2025-02-19 15:35:43 +0530

    first commit

--- myapp/src/Components/Hotels/Hotels.jsx
+++ myapp/src/Components/Hotels/Hotels.jsx
@@ -1,4 +1,8 @@
 import React from "react";
+import { IoMdHome } from "react-icons/io";
+import { FaRegHeart } from "react-icons/fa";
+import { CiViewTimeline } from "react-icons/ci";
+import { BiMessageAltDots } from "react-icons/bi";
 import { IoSearch } from "react-icons/io5";
 
 export const Hotels=()=>{
@@ -12,11 +16,36 @@ export const Hotels=()=>{
                 <div style={{flex:'1',textAlign:'center',justifyContent:'center',fontSize:'40px'}}><IoSearch /></div>
             </div>
             <div style={{border:'1px solid black',flex:'2'}}></div>
+            <div style={{border:'1px solid black',flex:'0.5'}}>
+                <a style={{fontFamily:'Arial,sans serif',fontSize:'25px',fontWeight:'bolder'}}>Popular Hotels</a>
+            </div>
             <div style={{border:'1px solid black',flex:'2'}}></div>
-            <div style={{border:'1px solid black',flex:'2'}}></div>
-            <div style={{border:'1px solid black',flex:'2'}}></div>
-            <div style={{border:'1px solid black',flex:'2'}}></div>
-            <div style={{border:'1px solid black',flex:'2'}}></div>
+            <div style={{border:'1px solid black',flex:'0.5'}}>
+                <a style={{fontFamily:'Arial,sans serif',fontSize:'25px',fontWeight:'bolder'}}>Low Cost Hotels</a>
+            </div>
+            <div style={{border:'1px solid black',flex:'1',display:'flex'}}>
+                <div style={{border:'1px solid black',flex:'1'}}></div>
+                <div style={{border:'1px solid black',flex:'2'}}>
+                    <a >Warwlck Grand Hotels</a><br/>
+                    <a>New Yak</a>
+                    <a>5.0<a>$480/night</a></a>water garder int
+                </div>
+            </div>
+            <div style={{border:'1px solid black',flex:'1.5',display:'flex',flexDirection:'column'}}>
+            <div style={{border:'1px solid black',flex:'1',display:'flex'}}>
+                <div style={{border:'1px solid black',flex:'1'}}></div>
+                <div style={{border:'1px solid black',flex:'2'}}>
+                    <a >Water Garder  Int </a>
+                </div>
+            </div>
+                <div style={{border:'1px solid black',flex:'1',display:'flex'}}>
+                    <div style={{border:'1px solid black',flex:'1'}}><IoMdHome /></div>
+                    <div style={{border:'1px solid black',flex:'1'}}><FaRegHeart /></div>
+                    <div style={{border:'1px solid black',flex:'1'}}><CiViewTimeline /></div>
+                    <div style={{border:'1px solid black',flex:'1'}}><BiMessageAltDots /></div>
+                    <div style={{border:'1px solid black',flex:'1'}}></div>
+                </div>
+            </div>
         </div>
     )
 }
