c831ee41d06739c7a7303d12abe36a42cbb9cf8e
cs22nagaratna <cs22nagaratna@svpkumta.in>
2025-02-21 12:49:37 +0530

    first commit

--- myapp/src/Components/Login/Login.jsx
+++ myapp/src/Components/Login/Login.jsx
@@ -1,10 +1,25 @@
 import react from 'react'
 import './Login.css'
+import { IoSearch } from "react-icons/io5";
 
 const Login=()=>{
     return(
-        <div className='box'>
-            <h1>nayana</h1>
+        <div className='container'>
+            <div className='header'>
+                <div className='header1'>
+                    <a>Let's</a>
+                    <span className='search'><IoSearch /></span>
+                </div>
+                <div className='header2'>
+                    <a>Explore Hotels</a>
+                </div>
+            </div>
+            <div className='box'>
+                <div className='box1'></div>
+                <div className='box1'></div>
+                <div className='box1'></div>
+                <div className='box1'></div>
+            </div>
         </div>
     )
 }
