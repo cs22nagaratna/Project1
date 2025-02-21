24b1f602612d722b87e1bd44cf217a37dabdeb3b
cs22nagaratna <cs22nagaratna@svpkumta.in>
2025-02-21 13:51:05 +0530

    first commit

--- myapp/src/Components/Login/Login.jsx
+++ myapp/src/Components/Login/Login.jsx
@@ -1,6 +1,10 @@
 import react from 'react'
 import './Login.css'
+import { FaHotel } from "react-icons/fa6";
 import { IoSearch } from "react-icons/io5";
+import { MdOutlineFlight } from "react-icons/md";
+import { IoLocationOutline } from "react-icons/io5";
+import { FaConciergeBell } from "react-icons/fa";
 
 const Login=()=>{
     return(
@@ -15,10 +19,29 @@ const Login=()=>{
                 </div>
             </div>
             <div className='box'>
-                <div className='box1'></div>
-                <div className='box1'></div>
-                <div className='box1'></div>
-                <div className='box1'></div>
+                <div className='box1'>
+                    <div className='im'><FaHotel /></div>
+                    <div className='te'>Hotel</div>
+                </div>
+                <div className='box2'>
+                    <div className='im'><MdOutlineFlight /></div>
+                    <div className='te'>Flight</div>
+                </div>
+                <div className='box2'>
+                    <div className='im'><IoLocationOutline /></div>
+                    <div className='te'>Place</div>
+                </div>
+                <div className='box2'>
+                    <div className='im'><FaConciergeBell /></div>
+                    <div className='te'>Food</div>
+                </div>
+            </div>
+            <div className='head'>
+                <h1>Popular Hotels</h1>
+                <a>See all</a>
+            </div>
+            <div className='hotel'>
+                
             </div>
         </div>
     )
