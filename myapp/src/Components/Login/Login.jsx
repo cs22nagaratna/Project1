1879d276dea017b80c9929cb29c7c23ee8bb6aa6
cs22nagaratna <cs22nagaratna@svpkumta.in>
2025-02-21 11:12:00 +0530

    first commit

---
+++ myapp/src/Components/Login/Login.jsx
@@ -0,0 +1,11 @@
+import react from 'react'
+import './Login.css'
+
+const Login=()=>{
+    return(
+        <div className='box'>
+            <h1>nayana</h1>
+        </div>
+    )
+}
+export default Login;
