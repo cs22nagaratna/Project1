9e41b8615855cba8f0311a3aa03f0fedeffeb3ad
cs22nagaratna <cs22nagaratna@svpkumta.in>
2025-02-21 10:01:50 +0530

    first commit

--- myapp/src/Components/Login/Login.stories.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import {Login} from "./Login"
-
-export default {
-    title:"Components/Login",
-    component:Login,
-};
-const Template=(args)=><Login {...args}/>;
-export const login=Template.bind()
