53e17de69509380e6716fc7ea40f71561fadc880
cs22nagaratna <cs22nagaratna@svpkumta.in>
2025-02-24 13:52:16 +0530

    first commit

--- myapp/src/Components/Dribble/Dribble.jsx
+++ myapp/src/Components/Dribble/Dribble.jsx
@@ -3,6 +3,14 @@ import "./Dribble.css"
 import { IoIosArrowForward } from "react-icons/io";
 import { GoBell } from "react-icons/go";
 import { IoSearch } from "react-icons/io5";
+import { RxDashboard } from "react-icons/rx";
+import { GoPaste } from "react-icons/go";
+import { FiUser } from "react-icons/fi";
+import { CiCalendar } from "react-icons/ci";
+import { IoSettingsOutline } from "react-icons/io5";
+import { GrCube } from "react-icons/gr";
+import { FaRegQuestionCircle } from "react-icons/fa";
+import { BiDockTop } from "react-icons/bi";
 
 const Dribble=()=>{
     return(
@@ -24,14 +32,42 @@ const Dribble=()=>{
                                 <a><GoBell /></a>
                             </div>
                             <div className="r">
-                                
+                                <div className="img">
+                                    <img src="https://cdn3.vectorstock.com/i/1000x1000/72/17/male-avatar-profile-icon-round-man-face-vector-18307217.jpg"/>
+                                </div>
+                                <div className="contant">
+                                    <a>Alexander Arnold</a><br/>
+                                    <a>as a Administrator </a>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
                 <div className="title">
-                    <div className="side-bar"></div>
-                    <div className="middle"></div>
+                    <div className="side-bar">
+                        <div className="side-bar1">
+                            <p><RxDashboard /></p>
+                            <p><GoPaste /></p>
+                            <p><FiUser /></p>
+                            <p><CiCalendar /></p>
+                            <p><GrCube /></p>
+                            <p><BiDockTop /></p>
+                        </div>
+                        <div className="sidebar">
+                            <p><FaRegQuestionCircle /></p>
+                            <p><IoSettingsOutline /></p>
+                        </div>
+                    </div>
+                    <div className="middle">
+                        <div className="c1"></div>
+                        <div className="c2"></div>
+                        <div className="c3"></div>
+                        <div className="c4"></div>
+                        <div className="c5"></div>
+                        <div className="c6"></div>
+                        <div className="c7"></div>
+                        <div className="c8"></div>
+                    </div>
                     <div className="last">
                         <div className="first"></div>
                         <div className="second">
