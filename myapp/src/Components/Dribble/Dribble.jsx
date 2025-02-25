d17d47896d582736c1c3b133a94a0a619d13e958
cs22nagaratna <cs22nagaratna@svpkumta.in>
2025-02-25 12:13:29 +0530

    first commit

--- myapp/src/Components/Dribble/Dribble.jsx
+++ myapp/src/Components/Dribble/Dribble.jsx
@@ -11,6 +11,9 @@ import { IoSettingsOutline } from "react-icons/io5";
 import { GrCube } from "react-icons/gr";
 import { FaRegQuestionCircle } from "react-icons/fa";
 import { BiDockTop } from "react-icons/bi";
+import { FaCircleArrowRight } from "react-icons/fa6";
+import { BsThreeDots } from "react-icons/bs";
+import { FaArrowRight } from "react-icons/fa6";
 
 const Dribble=()=>{
     return(
@@ -21,6 +24,7 @@ const Dribble=()=>{
                         <img src="https://png.pngtree.com/png-vector/20190507/ourmid/pngtree-vector-dental-icon-png-image_1025127.jpg"width={'60px'}height={'67px'}/>
                     </div>
                     <div className="list-customer">
+                    <a><FaCircleArrowRight /></a>
                         <div className="left">
                             <h3>List customer</h3>
                             <h3><IoIosArrowForward /></h3>
@@ -59,14 +63,79 @@ const Dribble=()=>{
                         </div>
                     </div>
                     <div className="middle">
-                        <div className="c1"></div>
-                        <div className="c2"></div>
-                        <div className="c3"></div>
-                        <div className="c4"></div>
-                        <div className="c5"></div>
-                        <div className="c6"></div>
-                        <div className="c7"></div>
-                        <div className="c8"></div>
+                        <div className="c1">
+                            <p>Patient Queue<a><IoSearch /></a></p>
+                            <a><button>All</button>     <button>Oldest</button>     <button>Newest</button></a>
+                        </div>
+                        <div className="c2">
+                            <div className="img1">
+                                <div className="g">
+                                    <img src="https://cdn-icons-png.flaticon.com/512/219/219969.png"width={'30px'}height={'30px'}/>
+                                </div>
+                                <div className="m"><BsThreeDots /></div>
+                            </div>
+                            <div className="name"><a>Bessie Cooper</a></div>
+                            <div className="regi">Register 12 March 2023<a><FaArrowRight /></a></div>
+                        </div>
+                        <div className="c3">
+                            <div className="img1">
+                                <div className="g">
+                                    <img src="https://cdn-icons-png.flaticon.com/512/219/219969.png"width={'30px'}height={'30px'}/>
+                                </div>
+                                <div className="m"><BsThreeDots /></div>
+                            </div>
+                            <div className="name"><a>Bessie Cooper</a></div>
+                            <div className="regi">Register 12 March 2023<a><FaArrowRight /></a></div>
+                        </div>
+                        <div className="c4">
+                            <div className="img1">
+                                <div className="g">
+                                    <img src="https://cdn-icons-png.flaticon.com/512/219/219969.png"width={'30px'}height={'30px'}/>
+                                </div>
+                                <div className="m"><BsThreeDots /></div>
+                            </div>
+                            <div className="name"><a>Bessie Cooper</a></div>
+                            <div className="regi">Register 12 March 2023<a><FaArrowRight /></a></div>
+                        </div>
+                        <div className="c5">
+                            <div className="img1">
+                                <div className="g">
+                                    <img src="https://cdn-icons-png.flaticon.com/512/219/219969.png"width={'30px'}height={'30px'}/>
+                                </div>
+                                <div className="m"><BsThreeDots /></div>
+                            </div>
+                            <div className="name"><a>Bessie Cooper</a></div>
+                            <div className="regi">Register 12 March 2023<a><FaArrowRight /></a></div>
+                        </div>
+                        <div className="c6">
+                            <div className="img1">
+                                <div className="g">
+                                    <img src="https://cdn-icons-png.flaticon.com/512/219/219969.png"width={'30px'}height={'30px'}/>
+                                </div>
+                                <div className="m"><BsThreeDots /></div>
+                            </div>
+                            <div className="name"><a>Bessie Cooper</a></div>
+                            <div className="regi">Register 12 March 2023<a><FaArrowRight /></a></div>
+                        </div>
+                        <div className="c7">
+                            <div className="img1">
+                                <div className="g">
+                                    <img src="https://cdn-icons-png.flaticon.com/512/219/219969.png"width={'30px'}height={'30px'}/>
+                                </div>
+                                <div className="m"><BsThreeDots /></div>
+                            </div>
+                            <div className="name"><a>Bessie Cooper</a></div>
+                            <div className="regi">Register 12 March 2023<a><FaArrowRight /></a></div>
+                        </div>
+                        <div className="c8">
+                            <div className="img1">
+                                <div className="g">
+                                    <img src="https://cdn-icons-png.flaticon.com/512/219/219969.png"width={'30px'}height={'30px'}/>
+                                </div>
+                                <div className="m"><BsThreeDots /></div>
+                            </div>
+                            <div className="name"><a>Bessie Cooper</a></div>
+                        </div>
                     </div>
                     <div className="last">
                         <div className="first">
@@ -80,13 +149,62 @@ const Dribble=()=>{
                         </div>
                         <div className="third">
                             <div className="ro1">
+                                <h3>History dental</h3>
                                 <div className="id">
-                                    <div className="t1"></div>
-                                    <div className="t2"></div>
-                                    <div className="t3"></div>
-                                    <div className="t4"></div>
-                                    <div className="t5"></div>
-                                    <div className="t6"></div>
+                                    <div className="t1">
+                                        <div className="d1">ID</div>
+                                        <div className="d2">Type</div>
+                                        <div className="d3">Treatment</div>
+                                        <div className="d4">Date</div>
+                                        <div className="d5">Result Treatment</div>
+                                        <div className="d6">Payment Status</div>
+                                        <div className="d7"></div>
+                                    </div>
+                                    <div className="t2">
+                                        <div className="d1">#12324</div>
+                                        <div className="d2"></div>
+                                        <div className="d3">Implant</div>
+                                        <div className="d4">12 Jun 2023</div>
+                                        <div className="d5">Well done</div>
+                                        <div className="d6">Pending</div>
+                                        <div className="d7"><BsThreeDots /></div>
+                                    </div>
+                                    <div className="t3">
+                                        <div className="d1">#20324</div>
+                                        <div className="d2"></div>
+                                        <div className="d3">Route canal</div>
+                                        <div className="d4">4 May 2023</div>
+                                        <div className="d5">Well done</div>
+                                        <div className="d6">Paid</div>
+                                        <div className="d7"><BsThreeDots /></div>
+                                    </div>
+                                    <div className="t4">
+                                        <div className="d1">#57686</div>
+                                        <div className="d2"></div>
+                                        <div className="d3">Dentures</div>
+                                        <div className="d4">2 Mar 2023</div>
+                                        <div className="d5">Well done</div>
+                                        <div className="d6">Paid</div>
+                                        <div className="d7"><BsThreeDots /></div>
+                                    </div>
+                                    <div className="t5">
+                                        <div className="d1">#68767</div>
+                                        <div className="d2"></div>
+                                        <div className="d3">Whitening</div>
+                                        <div className="d4">16 Feb 2023</div>
+                                        <div className="d5">Well done</div>
+                                        <div className="d6">Paid</div>
+                                        <div className="d7"><BsThreeDots /></div>
+                                    </div>
+                                    <div className="t6">
+                                        <div className="d1">#69696</div>
+                                        <div className="d2"></div>
+                                        <div className="d3">Implant</div>
+                                        <div className="d4">12 Jun 2023</div>
+                                        <div className="d5">Well done</div>
+                                        <div className="d6">Paid</div>
+                                        <div className="d7"><BsThreeDots /></div>
+                                    </div>
                                 </div>
                             </div>
                             <div className="ro2"></div>
