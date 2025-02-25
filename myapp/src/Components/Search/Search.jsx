22df73da33654399d90202c9e106d4346bb8d686
cs22nagaratna <cs22nagaratna@svpkumta.in>
2025-02-25 09:54:44 +0530

    first commit

--- myapp/src/Components/Search/Search.jsx
+++ myapp/src/Components/Search/Search.jsx
@@ -3,8 +3,21 @@ import "./Search.css"
 
 const Search=()=>{
     return(
-        <div className="container">
-
+        <div className="container1">
+            <div className="row1">
+                <div className="c1"><h1>O|x</h1></div>
+                <div className="c2"></div>
+                <div className="c3"></div>
+                <div className="c4">
+                    <div className="r1"></div>
+                    <div className="r2"></div>
+                    <div className="r3"></div>
+                    <div className="r4"></div>
+                    <div className="r5"></div>
+                    <div className="r6"></div>
+                </div>
+            </div>
+            <div className="row2"></div>
         </div>
     );
 }
